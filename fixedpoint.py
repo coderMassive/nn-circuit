00abab008ba31beb1d49aa08efbccf139da65077
Mark Raspopov <63547351+coderMassive@users.noreply.github.com>
2026-06-08 11:41:53 -0700

    refactor: removed unused fixed pointarray conversion functions

--- fixedpoint.py
+++ fixedpoint.py
@@ -3,14 +3,8 @@ from amaranth import *
 def to_fixed(val):
     return int(val * (2**16))
 
-def to_fixed_arr(arr):
-    return [to_fixed(val) for val in arr]
-
 def from_fixed(val):
     return val / 2**16
-
-def from_fixed_arr(arr):
-    return [val / 2**16 for val in arr]
 
 def fixed_mult(val1, val2):
     return (val1 * val2) >> 16
